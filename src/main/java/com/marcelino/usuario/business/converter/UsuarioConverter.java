4587fa79637507da8dc3d52b607f04a5f43025d6
Marcelino <pedro.marcelinodv@gmail.com>
2026-04-13 19:24:05 -0300

    Salva Usuario

--- src/main/java/com/marcelino/usuario/business/converter/UsuarioConverter.java
+++ src/main/java/com/marcelino/usuario/business/converter/UsuarioConverter.java
@@ -1,4 +1,93 @@
 package com.marcelino.usuario.business.converter;
 
+import com.marcelino.usuario.business.dto.EnderecoDTO;
+import com.marcelino.usuario.business.dto.TelefoneDTO;
+import com.marcelino.usuario.business.dto.UsuarioDTO;
+import com.marcelino.usuario.infrastructure.entity.Endereco;
+import com.marcelino.usuario.infrastructure.entity.Telefone;
+import com.marcelino.usuario.infrastructure.entity.Usuario;
+
+import java.util.List;
+
 public class UsuarioConverter {
+
+    public Usuario paraUsuario (UsuarioDTO usuarioDTO){
+        return Usuario.builder()
+                .nome(usuarioDTO.getNome())
+                .email(usuarioDTO.getEmail())
+                .senha(usuarioDTO.getSenha())
+                .enderecos(paraListEndereco(usuarioDTO.getEnderecos()))
+                .telefone(paraListTelefone(usuarioDTO.getTelefones()))
+                .build();
+    }
+
+    public List<Endereco> paraListEndereco(List<EnderecoDTO> enderecoDTOS){
+        return  enderecoDTOS.stream().map(this::paraEndereco).toList();
+    }
+
+    public Endereco paraEndereco ( EnderecoDTO enderecoDTO){
+        return Endereco.builder()
+                .cep(enderecoDTO.getCep())
+                .rua(enderecoDTO.getRua())
+                .cidade(enderecoDTO.getCidade())
+                .complemento(enderecoDTO.getComplemento())
+                .estado(enderecoDTO.getEstado())
+                .bairro(enderecoDTO.getBairro())
+                .numero(enderecoDTO.getNumero())
+                .build();
+    }
+
+    public List<Telefone> paraListTelefone(List<TelefoneDTO> telefoneDTOS){
+        return  telefoneDTOS.stream().map(this::paraTelefone).toList();
+    }
+
+    public Telefone paraTelefone(TelefoneDTO telefoneDTO){
+        return Telefone.builder()
+                .ddd(telefoneDTO.getDdd())
+                .numero(telefoneDTO.getNumero())
+                .build();
+    }
+
+
+
+    //convertor de dto para entety
+
+
+    public UsuarioDTO paraUsuarioDTO (Usuario usuarioDTO){
+        return UsuarioDTO.builder()
+                .nome(usuarioDTO.getNome())
+                .email(usuarioDTO.getEmail())
+                .senha(usuarioDTO.getSenha())
+                .enderecos(paraListEnderecoDTO(usuarioDTO.getEnderecos()))
+                .telefones(paraListTelefoneDTO(usuarioDTO.getTelefone()))
+                .build();
+    }
+
+    public List<EnderecoDTO> paraListEnderecoDTO(List<Endereco> enderecoDTOS){
+        return  enderecoDTOS.stream().map(this::paraEnderecoDTO).toList();
+    }
+
+    public EnderecoDTO paraEnderecoDTO ( Endereco enderecoDTO){
+        return EnderecoDTO.builder()
+                .cep(enderecoDTO.getCep())
+                .rua(enderecoDTO.getRua())
+                .cidade(enderecoDTO.getCidade())
+                .complemento(enderecoDTO.getComplemento())
+                .estado(enderecoDTO.getEstado())
+                .bairro(enderecoDTO.getBairro())
+                .numero(enderecoDTO.getNumero())
+                .build();
+    }
+
+    public List<TelefoneDTO> paraListTelefoneDTO(List<Telefone> telefoneDTOS){
+        return  telefoneDTOS.stream().map(this::paraTelefoneDTO).toList();
+    }
+
+    public TelefoneDTO paraTelefoneDTO(Telefone telefoneDTO){
+        return TelefoneDTO.builder()
+                .ddd(telefoneDTO.getDdd())
+                .numero(telefoneDTO.getNumero())
+                .build();
+    }
+
 }
--- src/main/java/com/marcelino/usuario/business/dto/EnderecoDTO.java
+++ src/main/java/com/marcelino/usuario/business/dto/EnderecoDTO.java
@@ -1,4 +1,22 @@
 package com.marcelino.usuario.business.dto;
 
+import jakarta.persistence.Column;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class EnderecoDTO {
+
+    private String rua;
+    private long numero;
+    private String bairro;
+    private String complemento;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+
 }
